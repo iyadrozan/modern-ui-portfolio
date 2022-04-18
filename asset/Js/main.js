b90198e1fc648d90e8e15e72897c75d2d02deb4c
iyadrozan <iyadrozan26@gmail.com>
2022-04-19 06:22:48 +0800

    contact form on progress

---
+++ asset/Js/main.js
@@ -0,0 +1,8 @@
+/*=============== Testimonial Swiper ===============*/
+var swiper = new Swiper('.testimonial-wrapper', {
+  loop: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
