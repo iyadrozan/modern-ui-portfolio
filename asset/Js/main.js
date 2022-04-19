ad75db3734d2fe72815dbc08dff588cccc17eccd
iyadrozan <iyadrozan26@gmail.com>
2022-04-20 00:49:35 +0800

    Contact on process

--- asset/Js/main.js
+++ asset/Js/main.js
@@ -1,6 +1,10 @@
 /*=============== Testimonial Swiper ===============*/
 var swiper = new Swiper('.testimonial-wrapper', {
   loop: 'true',
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
